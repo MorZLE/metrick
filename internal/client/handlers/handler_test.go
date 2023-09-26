8a0173db3ee50ea966328a6884487d89f0471323
Артем <art04.04@mail.ru>
2023-09-26 15:37:39 +0300

    4.2.2

--- internal/client/handlers/handler_test.go
+++ internal/client/handlers/handler_test.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -21,22 +20,6 @@ func TestHandler_Request(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			h := Handler{}
 			h.Request(tt.args.metric, tt.args.name, tt.args.val)
-		})
-	}
-}
-
-func TestNewHandler(t *testing.T) {
-	tests := []struct {
-		name string
-		want Handler
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewHandler(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewHandler() = %v, want %v", got, tt.want)
-			}
 		})
 	}
 }
